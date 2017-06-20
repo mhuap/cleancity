794b26e8cfcc8e28cd5305262e5bcd66fbe6ffc5
Wes <m.huapayabutron@mail.utoronto.ca>
2017-06-20 00:49:19 -0400

    main menu added. works to send to level1 - will need to change that to tutorial scene when tutorial is finished

---
+++ Clean City, Green City/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+	public void LoadStage()  {
+		Application.LoadLevel ("Level1");
+	}
+}
