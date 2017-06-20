3e447f81998fb36826dc11142e16cd010b02b8a4
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2017-06-20 19:25:56 -0400

    Added ability to zoom in on the city, but only in the grass/lake area. I tried zooming in on multiple areas but it didn't work properly; if I clicked on a part of the city to zoom in on it then that part as well as the lake part would zoom at the same time. Also made some space btwn recycling and compost bins

---
+++ Clean City, Green City/Assets/Scripts/ZoomCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCam : MonoBehaviour
+{	
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Camera> ().enabled = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (CityControl.zoomActivated == "y") {
+			GetComponent<Camera> ().enabled = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			GetComponent<Camera> ().enabled = false;
+			CityControl.zoomActivated = "n";
+		}
+	}
+}
