40ba2db4b89d0c639f3bfe1e12e3df92e8837dc8
Wes <m.huapayabutron@mail.utoronto.ca>
2017-06-20 22:14:17 -0400

    tutorial finished

---
+++ Clean City, Green City/Assets/Scripts/TutorialBins.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialBins : MonoBehaviour {
+
+	public Sprite cross;
+	public Sprite check;
+	public string binType = "Z";
+
+	private bool drop;
+	private List<string> bins =  new List<string> (new string[]{"C", "R", "T"});
+	private SpriteRenderer sr;
+	private Sprite oldSprite;
+	private Sprite newSprite;
+	private GameObject waste;
+
+	void Start(){
+		sr = gameObject.GetComponent<SpriteRenderer> ();
+		oldSprite = sr.sprite;
+		if (!bins.Contains (binType)) {
+			throw new System.ArgumentException ("Invalid binType", "binType");
+		}
+	}
+
+	void OnTriggerEnter (Collider other){
+		// other is waste being dragged into bins
+
+		waste = other.gameObject;
+		wasteComponent wc = waste.GetComponent<wasteComponent>();
+		bool rPoint = binType == "R" && wc.r;
+		bool cPoint = binType == "C" && wc.c;
+		bool tPoint = binType == "T" && wc.t;
+
+		if (rPoint || cPoint || tPoint) {	
+			newSprite = check;
+			drop = true;
+		} else {
+			newSprite = cross;
+		}
+
+		sr.sprite = newSprite;
+
+	}
+
+	void Update(){
+		if (drop && Input.GetMouseButtonUp(0)) {
+			Cursor.visible = true;
+			sr.sprite = oldSprite;
+			Destroy (waste);
+		}
+	}
+
+	void OnTriggerExit(){
+		sr.sprite = oldSprite;
+		drop = false;
+	}
+		
+}
---
+++ Clean City, Green City/Assets/Scripts/changeScene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class changeScene : MonoBehaviour {
+
+	public GameObject[] arrows;
+	public GameObject text;
+
+	private GameObject[] waste;
+	private int n;
+	private int lastN;
+
+	// Use this for initialization
+	void Start () {
+		waste = GameObject.FindGameObjectsWithTag ("waste");
+		lastN = waste.Length;
+		if (lastN != 4) {
+			throw new System.NotSupportedException ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waste = GameObject.FindGameObjectsWithTag ("waste");
+		n = waste.Length;
+		if (n != lastN) {
+			lastN = n;
+			if (n > 0) {
+				arrows [3 - n].SetActive (false);
+				arrows [4 - n].SetActive (true);
+				if (n == 1) {
+					text.SetActive (true);
+				}
+			} else {
+				// n == 0
+				SceneManager.LoadScene ("Level1");
+			}
+		}
+		
+	}
+}
