eba81be59848e453abad0424d54b8a1eec5f8192
mhuap <huapmar97@gmail.com>
2017-06-30 14:38:37 -0400

    .

---
+++ CleanCityGreenCity/Assets/Scripts/LandfillBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandfillBar : MonoBehaviour {
+
+	public int fillNumber;
+	public int 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
