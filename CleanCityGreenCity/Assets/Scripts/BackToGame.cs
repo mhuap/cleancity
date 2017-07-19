9e33a8ac2307a5c18112f194fc52ca62e829cf44
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2017-07-19 00:13:38 -0400

    added Reference scene with details, added button to altLevel1 scene to reference page.

---
+++ CleanCityGreenCity/Assets/Scripts/BackToGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackToGame : MonoBehaviour {
+
+
+	private Button btg;
+
+	// Use this for initialization
+	void Start () {
+		btg = GetComponent<Button> ();
+		btg.onClick.AddListener (() => ClickBackToGame ());
+	}
+	
+	// Update is called once per frame
+	public void ClickBackToGame () {
+		SceneManager.LoadScene ("altLevel1");
+	}
+}
---
+++ CleanCityGreenCity/Assets/Scripts/RefPageButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RefPageButton : MonoBehaviour {
+
+	private Button goToRef;
+
+	// Use this for initialization
+	void Start ()
+	{
+		goToRef = GetComponent<Button> ();
+		goToRef.onClick.AddListener (() => ClickRefButton ());
+	}
+
+	// Update is called once per frame
+	public void ClickRefButton ()
+	{
+		SceneManager.LoadScene ("Reference");
+	}
+}
